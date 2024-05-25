a104f6e31ef3431474f4a298bf6c9da43120f6d4
DOGBATSE Kossi Innocent <dogbatseinnocent@gmail.com>
2024-05-25 21:46:26 +0000

    A vous de jouer

--- Scraping/main.py
+++ Scraping/main.py
@@ -19,15 +19,15 @@ url1 = "https://www.google.com"
    # f.write(response.text)
 
 
-url2 = "https://books.toscrape.com/"
-response = requests.get(url2)
-soup = BeautifulSoup(response.text, 'html.parser')
-#image = soup.find_all('img', alt_="The Black Maria")
-aside = soup.find('div', class_="side_categories")
-categories_div = aside.find("ul").find("li").find("ul")
-categories = [chield.text.strip() for chield in categories_div if chield.name ]
+# url2 = "https://books.toscrape.com/"
+# response = requests.get(url2)
+# soup = BeautifulSoup(response.text, 'html.parser')
+# image = soup.find_all('img', alt_="The Black Maria")
+# aside = soup.find('div', class_="side_categories")
+# categories_div = aside.find("ul").find("li").find("ul")
+# categories = [chield.text.strip() for chield in categories_div if chield.name ]
 
-print(categories)
+# print(categories)
 
 
 #for categorie in categories.children:
@@ -37,5 +37,45 @@ print(categories)
 
 #print(aside.children)
 
+# url2 = "https://books.toscrape.com/"
 
+# response = requests.get(url2)
+
+# soup = BeautifulSoup(response.text, 'html.parser')
+
+# images = soup.find('section').find_all('img')
+
+# for image in images:
+#     print(image['src'])
+
+# url = 'https://books.toscrape.com/'
+
+# response = requests.get(url)
+
+# with open('Scraping/index.html', 'w') as f:
+#     f.write(response.text)
+
+# with open('Scraping/index.html', 'r') as f:
+#     html = f.read()
+
+# soup = BeautifulSoup(html, 'html.parser')
+
+# images = soup.find('section').find_all('img')
+
+# for image in images:
+#     print(image.get('src'))
+
+
+url2 = "https://books.toscrape.com/"
+
+response = requests.get(url2)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+articles = soup.find_all('article', class_='product_pod')
+
+for image in articles:
+    links = image.find_all('a')
+    if len(links) >= 2:
+        link = links[1]
+        print(link.get('title'))
 
