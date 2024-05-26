c93f68aca7767835d8039ae7ad47aa188d32d480
DOGBATSE Kossi Innocent <dogbatseinnocent@gmail.com>
2024-05-26 13:00:52 +0000

    Ma proposition de solution de l'exercice

--- Scraping/main.py
+++ Scraping/main.py
@@ -66,16 +66,54 @@ url1 = "https://www.google.com"
 #     print(image.get('src'))
 
 
-url2 = "https://books.toscrape.com/"
+# url2 = "https://books.toscrape.com/"
 
-response = requests.get(url2)
+# response = requests.get(url2)
 
-soup = BeautifulSoup(response.text, 'html.parser')
-articles = soup.find_all('article', class_='product_pod')
+# soup = BeautifulSoup(response.text, 'html.parser')
+# articles = soup.find_all('article', class_='product_pod')
 
-for image in articles:
-    links = image.find_all('a')
-    if len(links) >= 2:
-        link = links[1]
-        print(link.get('title'))
+# for image in articles:
+#     links = image.find_all('a')
+#     if len(links) >= 2:
+#         link = links[1]
+#         print(link.get('title'))
+
+url = 'https://books.toscrape.com/'
+
+categories_dict = {}
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+categories_name = soup.find('aside').find('ul').find('li').find('ul')
+
+list_cat_name = [chield.text.strip() for chield in categories_name.children if chield.name]
+
+hrefs = categories_name.find_all('a')
+
+list_cat_href = [url + chield['href'] for chield in hrefs]
+
+categories_href = {key: value for key, value in zip(list_cat_name, list_cat_href)}
+
+Seuil_de_livre = int(input("Afficher les catégories qui contiennent moins de : "))
+
+for key, href in categories_href.items():
+
+    response_url = requests.get(href)
+
+    soup_url = BeautifulSoup(response_url.text, 'html.parser')
+
+    liste_livre = soup_url.find('section').find('ol', class_='row')
+
+    nombre_livre = len([li for li in liste_livre.children])
+
+    if nombre_livre < Seuil_de_livre:
+        print(key, nombre_livre)
+
+
+
+
+
 
