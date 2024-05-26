255ff7c6e36f64314877b7e9b0d099b02bff52c3
DOGBATSE Kossi Innocent <dogbatseinnocent@gmail.com>
2024-05-26 15:06:45 +0000

    Ajout de la proposition de correection de Docstring

--- Scraping/main.py
+++ Scraping/main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from pprint import pprint
+from urllib.parse import urljoin
 
 url1 = "https://www.google.com"
 
@@ -79,41 +80,70 @@ url1 = "https://www.google.com"
 #         link = links[1]
 #         print(link.get('title'))
 
-url = 'https://books.toscrape.com/'
 
-categories_dict = {}
+###############################################
+#######    Ma proposition de correction #######
+###############################################
 
-response = requests.get(url)
+# url = 'https://books.toscrape.com/'
 
-soup = BeautifulSoup(response.text, "html.parser")
+# categories_dict = {}
 
-categories_name = soup.find('aside').find('ul').find('li').find('ul')
+# with requests.Session() as session:
+#     response = session.get(url)
+#     soup = BeautifulSoup(response.text, "html.parser")
+#     categories_name = soup.find('aside').find('ul').find('li').find('ul')
+#     list_cat_name = [chield.text.strip() for chield in categories_name.children if chield.name]
+#     hrefs = categories_name.find_all('a')
+#     list_cat_href = [urljoin(url, chield['href']) for chield in hrefs]
+#     categories_href = {key: value for key, value in zip(list_cat_name, list_cat_href)}
+#     Seuil_de_livre = int(input("Afficher les catégories dans un seuil de : "))
 
-list_cat_name = [chield.text.strip() for chield in categories_name.children if chield.name]
-
-hrefs = categories_name.find_all('a')
-
-list_cat_href = [url + chield['href'] for chield in hrefs]
-
-categories_href = {key: value for key, value in zip(list_cat_name, list_cat_href)}
-
-Seuil_de_livre = int(input("Afficher les catégories qui contiennent moins de : "))
-
-for key, href in categories_href.items():
-
-    response_url = requests.get(href)
-
-    soup_url = BeautifulSoup(response_url.text, 'html.parser')
-
-    liste_livre = soup_url.find('section').find('ol', class_='row')
-
-    nombre_livre = len([li for li in liste_livre.children])
-
-    if nombre_livre < Seuil_de_livre:
-        print(key, nombre_livre)
+#     for key, href in categories_href.items():
+#         response_url = session.get(href)
+#         soup_url = BeautifulSoup(response_url.text, 'html.parser')
+#         liste_livre = soup_url.find('section').find('ol', class_='row')
+#         nombre_livre = len([li for li in liste_livre.children])
+#         if nombre_livre < Seuil_de_livre:
+#             print(f"La catégories '{key}' ne contient que ({nombre_livre}) livres")
+#         else:
+#             print(f"La catégories '{key}' contient assez de livre ({nombre_livre})")
 
 
+###############################################
+############ # Solution Docstring #############
+###############################################
 
+BASE_URL = "https://books.toscrape.com/index.html"
+
+
+def main(threshold: int = 5):
+    with requests.Session() as session:
+        response = session.get(BASE_URL)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        categories = soup.find('ul', class_="nav nav-list").find_all('a')
+
+        # Alternative
+        categories = soup.select('ul.nav.nav-list a')
+        categories_urls = [category['href'] for category in categories]
+
+        # Go to all categories page
+        for category_url in categories_urls:
+            full_url = urljoin(BASE_URL, category_url)
+            response = session.get(full_url)
+            soup = BeautifulSoup(response.text, 'html.parser')
+
+            books = soup.find_all('article', class_="product_pod")
+            books = soup.select('article.product_pod')
+            category_title = soup.find("h1").text
+            number_of_books = len(books)
+            if number_of_books <= threshold:
+                print(f"La catégorie '{category_title}' ne contient pas assez de livres ({number_of_books})")
+
+
+if __name__ == '__main__':
+    main(threshold=5)
 
 
 
